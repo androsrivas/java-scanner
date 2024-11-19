7d7ba18da82de61b3fdb7680f5ca6502c2f3119d
dustisme <androsrivas@outlook.com>
2024-11-19 16:20:32 +0100

    finish bot exercise

--- src/Bot.java
+++ src/Bot.java
@@ -1,33 +1,45 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
         //Crea el scanner
-
-        System.out.println("Hola, ¿Cómo te llamas?");
-
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
-        System.out.println("Hola <name>! Soy un Bot. ¿De qué ciudad eres?");
-
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
-        System.out.println("He escuchado que <location> es un encanto. Yo nací en Oracle city.");
-        System.out.println("\n¿Cuántos años tienes?");
-
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
-        System.out.println("Entonces me dices que tienes <age> años. Yo tengo 400 años.");
-        System.out.println("Eso quiere decir que soy <divide 400 por tu edad> veces más vieja que tu.");
-        System.out.println("Basta de hablar de mi misma. ¿Cuál es tu lenguaje de programación favorito? (Por favor no digas Python)");
-
         //Recoge el dato
         //Añade una nueva línea.
-        System.out.println("<language>, ¡Eso es excelente! Mucho gusto haber hablado contigo <name>, ¡Hablamos luego!");
-
         //¿Te acordaste del nextLine trap?
         //Recuerda cerrar el scanner
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hola, ¿cómo te llamas?");
+        String name = input.nextLine();
+        System.out.println("Encantade de conocerte " + name);
+
+        System.out.println("¿desde dónde programas?");
+        String location = input.nextLine();
+        System.out.println("Qué hermosa ciudad es " + location);
+
+        System.out.println("¿y cuántos años tienes?");
+        int age = input.nextByte();
+        input.nextLine();
+
+        if (age < 27) {
+            System.out.println("Qué joven eres!");
+        } else if (age < 40) {
+            System.out.println("Debes ser una persona muy experimentada");
+        } else {
+            System.out.println("Rezumas sabiduría");
+        }
+
+        System.out.println("por último, ¿cuál es tu lenguaje de programación favorito?");
+        String programmingLanguage = input.nextLine();
+        System.out.println(programmingLanguage + " es un buen lenguaje de programación. Buena elección.");
+
+        input.close();
     }
 }
